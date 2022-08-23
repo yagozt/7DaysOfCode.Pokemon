06313efd3baca764f3fb01a66c07665b8f1b3e73
Yago Henrique <yagozt.henrique@gmail.com>
2022-08-23 09:04:45 -0300

    First commit of 7DaysOfCode with pokemon API

---
+++ src/7DaysOfCode.Pokemon.API/Controllers/PokeApiDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace _7DaysOfCode.Pokemon.API.Controllers
+{
+    public class PokeApiDTO
+    {
+        public PokeApiDTO()
+        {
+            Results = Array.Empty<PokemonDTO>();
+        }
+
+        public int Count { get; set; }
+        public string Next { get; set; } = string.Empty;
+        public string Previous { get; set; } = string.Empty;
+        public PokemonDTO[] Results { get; set; }
+
+        public class PokemonDTO
+        {
+            public string Name { get; set; } = string.Empty;
+            public string Url { get; set; } = string.Empty;
+        }
+    }
+}
---
+++ src/7DaysOfCode.Pokemon.API/Controllers/PokemonController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace _7DaysOfCode.Pokemon.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetPokemons(CancellationToken cancellationToken)
+        {
+            using var client = new HttpClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://pokeapi.co/api/v2/pokemon");
+            var responseResult = await client.SendAsync(requestMessage, cancellationToken);
+            using var contentStream = await responseResult.Content.ReadAsStreamAsync(cancellationToken);
+            var contentJsonResult = await JsonSerializer.DeserializeAsync<PokeApiDTO>(contentStream, new JsonSerializerOptions {  PropertyNamingPolicy = JsonNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true },cancellationToken: cancellationToken);
+
+            return Ok(contentJsonResult);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPokemon([FromRoute]int id, CancellationToken cancellationToken)
+        {
+            using var client = new HttpClient();
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, $"https://pokeapi.co/api/v2/pokemon/{id}");
+            var responseResult = await client.SendAsync(requestMessage, cancellationToken);
+            var contentString = await responseResult.Content.ReadAsStringAsync(cancellationToken);
+
+            return Ok(contentString);
+        }
+    }
+}
